1069ccfef9823d86d0d8f99bc0fd15d90f52c619
Doki <dorianmarasovic7@gmail.com>
2023-05-25 01:12:51 +0200

    Main2

--- app/src/main/java/com/example/gods_diary1/MainActivity2.java
+++ app/src/main/java/com/example/gods_diary1/MainActivity2.java
@@ -3,6 +3,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,15 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ImageView imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform actions when the image button is clicked
+                // For example, you can launch another activity or show a toast message
+                Toast.makeText(getApplicationContext(), "Image button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -38,4 +49,5 @@ public class MainActivity2 extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
