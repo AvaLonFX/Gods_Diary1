4b35495858fbe9e31d1ab544b2567fb4f22fcd53
Filip <fpapic2000@gmail.com>
2023-05-24 03:39:30 +0200

    aasdsad

--- app/src/main/java/com/example/gods_diary1/MainActivity2.java
+++ app/src/main/java/com/example/gods_diary1/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.example.gods_diary1;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +12,30 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_item1) {
+            // Handle menu item 1 click
+            Toast.makeText(this, "Menu Item 1 selected", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.menu_item2) {
+            // Handle menu item 2 click
+            Toast.makeText(this, "Menu Item 2 selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
